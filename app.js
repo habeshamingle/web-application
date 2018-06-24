04fdd519d0d91ee2731ded58b3a6bcfecb0d9a87
amanbahata <bahataaman@gmail.com>
2018-06-24 21:21:18 +0100

    Adds user authentication

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+
 require('./app_api/models/db');
+require('./app_api/config/passport');
 
 const routes = require('./app_server/routes/index');
 const routesApi = require('./app_api/routes/index');
@@ -21,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/api', routesApi);
