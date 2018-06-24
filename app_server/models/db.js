2004e3d18ba2bc8fc52691a2617e2e1062994a01
amanbahata <bahataaman@gmail.com>
2018-06-24 10:53:40 +0100

    First commit

---
+++ app_server/models/db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://localhost/habeshamingle';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+   console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+   console.log('Mongoose connection error ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+   console.log('Mongoose disconnected');
+});
+
+/**
+ * Reusable function to close Mongoose connection
+ * @param msg
+ * @param callback
+ */
+var gracefulShutDown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnectedthrough ' + msg);
+    });
+};
+
+/**
+ * Listen for Node processes for termination or restart signals, and call
+ * gracefulShutDown function when appropriate, passing a continuation callback
+ */
+// for Nodemon restarts
+process.once('SIGUSR2', function () {
+    gracefulShutDown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// for App termination
+process.on('SIGINT', function () {
+   gracefulShutDown('app termination', function () {
+       process.exit(0);
+   });
+});
+//For Heroku app termination
+process.on('SIGTERM', function () {
+    gracefulShutDown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
---
+++ app_server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/home');
+
+
+/* GET home page. */
+router.get('/', ctrlMain.home);
+
+module.exports = router;
